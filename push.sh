24e6991ba6cb39a8de3bff939d828fd750ebee9f
kev-devops <kevin.bomme@campus.academy>
2020-11-27 14:58:25 +0100

    master

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git remote add cleverapps
-https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_0b3466d5-bf00-4da7-818a-18da318af0d1.git
+https://$CLEVER_TOKEN:$CLEVER_SECRET@push.clever-cloud.com/app_YOUR_APPLICATION_ID.git
 git --verbose --force push cleverapps master
